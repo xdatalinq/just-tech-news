92e87cd800cf0c7612f12020d297b7b566fb9cf4
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-27 18:49:50 -0400

    code cleanup

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-app.use(routes);
+app.use(routes); 
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
