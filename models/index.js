bb03217acf93a65dd2f66b11a98c11bec16001f2
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-27 19:19:19 -0400

    code cleanup

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -37,22 +38,6 @@ User.hasMany(Vote, {
 });
 
 Post.hasMany(Vote, {
-  foreignKey: 'post_id'
-});
-
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -15,32 +15,31 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
-  attributes: { exclude: ['password'] },
-  where: {
-    id: req.params.id
-  },
-  include: [
-    {
-      model: Post,
-      attributes: ['id', 'title', 'post_url', 'created_at']
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
     },
-    // include the Comment model here:
-    {
-      model: Comment,
-      attributes: ['id', 'comment_text', 'created_at'],
-      include: {
+    include: [
+      {
         model: Post,
-        attributes: ['title']
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
       }
-    },
-    {
-      model: Post,
-      attributes: ['title'],
-      through: Vote,
-      as: 'voted_posts'
-    }
-  ]
-})
+    ]
+  })
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
