6ab15fe8257468226014314228525a5bfa9db0eb
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-28 13:08:00 -0400

    code cleanup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -27,8 +29,8 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));  
-      // pass a single post object into the homepage template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
       res.render('homepage', { posts });
     })
     .catch(err => {
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+ 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on connection to db and server
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
